324e94e435e65708f2651f5733216dd92811fc7d
Kaspar Naaber <kaspar.naaber@outlook.com>
2015-06-08 14:05:12 +0300

    Remove deprecated funtcions and gallery hover effect

--- javascripts/src/concat/main.js
+++ javascripts/src/concat/main.js
@@ -353,13 +353,4 @@
         }
     });
 
-    // Reduces opacity of the gallery images that are not under the cursor.
-    $('.edys-gallery').mouseover(function() {
-      $(this).find('.edys-gallery-item').addClass('inactive');
-    });
-
-    $('.edys-gallery').mouseout(function() {
-      $(this).find('.edys-gallery-item').removeClass('inactive');
-    });
-
 })(jQuery);
